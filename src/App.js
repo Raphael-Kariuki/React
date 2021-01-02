e4a45823ed4f2168dbe066a976a14d797008933a
m0ckinjay <m0ckinjay@yandex.com>
2021-01-02 23:14:42 +0300

    cyberpunk ongrid layout

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <nav className="Navigation">
+          <ul>
+            <li><a href="#"><span>Home</span></a></li>
+            <li><a href="#"><span>Gallery</span></a></li>
+          </ul>
+        </nav>
+        <div className="Content">
+        <div className="bg1"></div>
+        <div className="bg1"></div>
+        <div className="bg1"></div>
+        <div className="bg1"></div>
+        <div className="bg1"></div>
+        <div className="bg1"></div>
+        <div className="bg1"></div>
+        <div className="bg1"></div>
+        </div>
+    </div>
+  );
+}
+
+export default App;
